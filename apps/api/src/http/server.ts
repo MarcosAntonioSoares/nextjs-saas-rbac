22e68dc5f56003854268222f1cd7f6ba896df840
Marcos Soares <marcosas.soares2@gmail.com>
2025-11-30 18:04:42 -0300

    Created member routes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -5,6 +5,9 @@ import { createAccount } from '@/http/routes/auth/create-account'
 import { getProfile } from '@/http/routes/auth/get-profile'
 import { requestPasswordRecover } from '@/http/routes/auth/request-password-recover'
 import { resetPassword } from '@/http/routes/auth/reset-password'
+import { getMembers } from '@/http/routes/members/get-members'
+import { removeMember } from '@/http/routes/members/remove-member'
+import { updateMember } from '@/http/routes/members/update-members'
 import { createOrganization } from '@/http/routes/orgs/create-organization'
 import { getMembership } from '@/http/routes/orgs/get-membership'
 import { getOrganization } from '@/http/routes/orgs/get-organization'
@@ -87,6 +90,10 @@ app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
 app.register(updateProject)
+
+app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running')
